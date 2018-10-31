53e7210b2a290646c232bddb3624c33f493f734c
IlyaVerameichyk <ilya_verameichyk@epam.com>
2018-10-31 12:26:49 +0300

    WIP

--- d2d3/4.WindowsServices/WatcherService/Service/Program.cs
+++ d2d3/4.WindowsServices/WatcherService/Service/Program.cs
@@ -1,4 +1,6 @@
-﻿using Topshelf;
+﻿using System;
+using System.IO;
+using Topshelf;
 
 namespace Service
 {
@@ -6,7 +8,25 @@ namespace Service
     {
         public static void Main(string[] args)
         {
-            HostFactory.Run(x => x.Service<Service>());
+            HostFactory.Run(x =>
+            {
+                x.Service<Service>(s =>
+                {
+                    try
+                    {
+                        s.ConstructUsing(name => new Service());
+                        s.WhenStarted(ts => ts.Start(null));
+                        s.WhenStopped(ts => ts.Stop(null));
+                    }
+                    catch (Exception e)
+                    {
+                        System.IO.File.WriteAllText("C:\\Users\\Ilya_Verameichyk\\Desktop\\123.txt", e.ToString());
+                    }
+                });
+
+                System.IO.File.WriteAllText("C:\\Users\\Ilya_Verameichyk\\Desktop\\123.txt", "123");
+
+            });
         }
     }
 }
